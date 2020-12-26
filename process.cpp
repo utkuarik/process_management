9488a10ce1bcf4045185386f39c568c364041642
Semih Serhat Karakaya <karakayasemi@itu.edu.tr>
2020-12-26 05:07:49 -0800

    first half working version

--- process.cpp
+++ process.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include<iostream>
 #include <unistd.h>
@@ -8,19 +7,17 @@
 #include <sstream>
 #include <fstream>
 #include <string>
-#include <string.h>
 #include <csignal>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <map>
 #include <vector>
 #include <signal.h> 
-using namespace std;
 
+using namespace std;
 
 // define signal handlers
 void sighup_handler(int sighup) 
-  
 { 
     signal(SIGHUP, sighup_handler ); /* reset signal */
     printf("CHILD: I have received a SIGHUP\n"); 
@@ -28,7 +25,6 @@ void sighup_handler(int sighup)
   
 // sigint() function definition 
 void sigint_handler(int sigint) 
-  
 { 
     signal(SIGINT, sigint_handler); /* reset signal */
     printf("CHILD: I have received a SIGINT\n"); 
@@ -46,50 +42,36 @@ void sigquit_handler(int sigquit)
 
 int main(int argc, char *argv[]) 
 { 
+    vector<string> file_content;
+    fstream process_file (argv[2], fstream::app);
 
-vector<string> file_content;
-fstream process_file ("./pr_output.txt", fstream::out | fstream::app);
+    string line;
+    char* child_process = argv[1];
 
-string line;
-char* child_process = argv[1];
+    cout<<"file created"<<endl;
+    while (!process_file.eof())
+    {
+        
+        getline (process_file,line);   
+        cout<<line<<endl;
+        file_content.push_back(line);    
+    }
 
+    for(int i=0; i < file_content.size(); i++){
 
+        cout << file_content.at(i)<< endl;
+        process_file<<file_content.at(i)<<endl;
+    }
 
-
-// cout << argv[1]<<" is waiting for a signal"<<endl;
-
-
-cout<<"file created"<<endl;
-while (!process_file.eof())
-{
-	
-	getline (process_file,line);   
-	cout<<line<<endl;
-	file_content.push_back(line);    
-}
-process_file.close();
-
-for(int i=0; i < file_content.size(); i++){
-
-	cout << file_content.at(i)<< endl;
-	process_file<<file_content.at(i)<<endl;
-}
+    process_file << argv[1]<<" is waiting for a signal"<<endl;
+    cout << argv[1]<<" is started and it has a pid of "<<to_string(getpid())<<endl;
 
 
-process_file.open("./pr_output.txt", fstream::out | fstream::app);
-cout<<"yesssss"<<endl;
-process_file << argv[1]<<" is waiting for a signal"<<endl;
-cout << argv[1]<<" is started and it has a pid of "<<to_string(getpid())<<endl;
-
-
-signal(SIGHUP, sighup); 
-signal(SIGINT, sigint); 
-signal(SIGQUIT, sigquit); 
-for (;;) 
-    ; /* loop for ever */
-
-return 0;
-
+    signal(SIGHUP, sighup_handler); 
+    signal(SIGINT, sigint_handler); 
+    signal(SIGQUIT, sigquit_handler); 
+    for (;;) ; /* loop for ever */
+    return 0;
 
 }
 
--- watchdog.cpp
+++ watchdog.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include<iostream>
 #include <unistd.h>
@@ -13,109 +12,54 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <map>
-// #include <libexplain/execlp.h>
+#include <vector>
 using namespace std;
-
-// int main(int argc, char* argv[])
-// {
-//     pid_t child_pid;
-//     int number_of_processes = stoi(argv[1]);
-//     for(int i = 1; i <= number_of_processes; i++){                    
-//         child_pid = fork();
-//         if(child_pid <= 0){
-//             break;
-//         }
-
-
-        
-//     }
-
-//     cout << i << " child process id: " << getpid()<< " " << "parent process id: "<< getppid() << endl;
-
-
-// }
-
-// int main (int argc, char *argv[]) {
-//    pid_t childpid = 0; 
-//    int i, n;
- 
-//    if (argc != 2){   /* check for valid number of command-line arguments */ 
-//       fprintf(stderr, "Usage: %s processes\n", argv[0]);
-//       return 1; 
-//    }     
-//    n = atoi(argv[1]);  
-//     for (i = 1; i < n; i++)
-//       if ((childpid = fork()) <= 0)
-//          break;
- 
-//     fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
-//            i, (long)getpid(), (long)getppid(), (long)childpid);
-//    return 0; 
-// }
 
 int main(int argc, char *argv[]) 
 { 
-
+    vector<int> pidList;
     pid_t pid ;
-    char * myfifo = (char*) "/tmp/myfifo";
+    char *myfifo = (char*) "/tmp/myfifo";
 
     int namedpipe = open(myfifo, O_WRONLY);
-    int fd;
-    char arr1[80], arr2[80];
     fstream watchdog_file(argv[3], ios::out);
     fstream process_file (argv[2], ios::out);
-    
-        if(!watchdog_file)
-    {
-        cout << "Couldn't open file" << endl;
-        return 1;
-    }
 
     // Add watchdog to pipe 
     string  msg;
     msg =  "P0 "  + to_string(getpid());
     cout << msg << endl;
     write(namedpipe, msg.c_str(), strlen(msg.c_str())+1); 
-    close(namedpipe);
     
 
-
-
     int number_of_processes = stoi(argv[1]);
-    for(int i=1;i<=number_of_processes;i++) // loop will run n times (n=5) 
+    for(int i=1 ; i<=number_of_processes; i++) // loop will run n times (n=5) 
     { 
         pid = fork();
         if(pid == 0) 
-        { 
-
-            string  msg;
+        {
+            pidList.push_back(getpid());
             msg =  "P" + to_string(i) + " " + to_string(getpid());
             cout << msg << endl;
             string temp = "P" + to_string(i);
             const char *child_process = temp.c_str();
             write(namedpipe, msg.c_str(), strlen(msg.c_str())+1); 
-            close(namedpipe);
 
             cout<<"P"<<i<<" is started and it has a pid of "<<to_string(getpid())<<endl;
             watchdog_file << "P"<<i<<" is started and it has a pid of "<<to_string(getpid())<<endl;
 
 
             cout<< child_process << endl;
-            int i = execlp("./process","./process", child_process, NULL);
-            cout<<i<<endl;
+            int k = execlp("./process", "./process", child_process, argv[2], NULL);
+            cout<<k<<endl;
             return 0;
-
-            
-        } 
-
-        else if(pid > 0){
-
-        wait(NULL); /* parent will wait for the child to complete */ 
-        printf("I am the parent process.\n");
         }
-    } 
-    for(int i=0;i<number_of_processes;i++) // loop will run n times (n=5) 
-    wait(NULL); 
-      
+    }
+    int count = 0; 
+    while(count < 5) {
+        int deadChildPid = wait(NULL); /* parent will wait for the child to complete */ 
+        cout << "dead pid:" <<deadChildPid<< endl;
+        count++;
+    }
 } 
 
